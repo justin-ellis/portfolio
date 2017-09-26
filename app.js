ab4ee1eed6bbc6bf92313159786a883112fb27bf
Jellis <venetianself@hotmail.com>
2017-09-26 03:52:27 -0500

    added brand statement modal

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+$(() => {
+
 console.log('connected');
+
+$('#aboutMe').on('click', (e) =>{
+	$('#modal1div').css('display', 'block');
+	$('#modal1divcontainer').css('display', 'block');
+});
+
+
+$('#modal1button').on('click', (e) => {
+$('#modal1div').css('display', 'none');
+$('#modal1divcontainer').css('display', 'none');
+});
+
+});
